8c2e81e837ae24793c57c56800f9e49de2776ae6
pabra <patrick+github-commit-2020@braune.xyz>
2020-06-23 13:37:49 +0200

    move types into separate file

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,16 @@
-// https://en.wikipedia.org/wiki/Syslog#Severity_level
-const logLevels = {
-  emerg: 0,
-  alert: 1,
-  crit: 2,
-  err: 3,
-  warning: 4,
-  notice: 5,
-  info: 6,
-  debug: 7,
-} as const;
-
-type LogLevels = typeof logLevels;
-type LoggerName = string;
-type NameChain = Readonly<LoggerName[]>;
-type Logger = {
-  readonly level: keyof LogLevels;
-  readonly name: LoggerName;
-  readonly nameChain: NameChain;
-};
-type MessageRaw = string;
-interface Message {
-  readonly raw: MessageRaw;
-  readonly level: keyof LogLevels;
-}
-interface MessageFormatted extends Message {
-  readonly formatted: string;
-}
-
-// a Filter decides if current call should be logged or not
-type Filter = (logger: Logger, message: Message) => boolean;
-
-// a Formatter formats the raw message into a string that will be logged
-type Formatter = (logger: Logger, message: Message) => MessageFormatted;
-
-// a Transporter takes the formatted message and "transports" it to console, filesystem, network, etc
-type Transporter = (logger: Logger, message: MessageFormatted) => void;
-
-// a Handler bundles Filter, Formatter and Transporter
-type Handler = {
-  readonly filter: Filter;
-  readonly formatter: Formatter;
-  readonly transporter: Transporter;
-};
-type Handlers = Readonly<Handler[]>;
+import {
+  Filter,
+  Formatter,
+  GetLogger,
+  GetLoggerReturn,
+  GetLoggerWrapper,
+  Handler,
+  Handlers,
+  Logger,
+  Message,
+  MessageRaw,
+  Transporter,
+} from './types';
 
 const maxLevelFilter: Filter = (logger, msg) => msg.level <= logger.level;
 
@@ -74,23 +42,6 @@ const logHandlers = (logger: Logger, msg: Message, handlers: Handlers) => {
     log(logger, msg, handler);
   }
 };
-
-type GetLoggerArgs = {
-  readonly name: string;
-  readonly level: keyof LogLevels;
-  readonly handlers: Handlers;
-  readonly nameChain: NameChain;
-};
-type GetLoggerReturn = { [K in keyof LogLevels]: (msg: MessageRaw) => void } & {
-  getLogger: GetLoggerWrapper;
-};
-type GetLogger = (args: GetLoggerArgs) => GetLoggerReturn;
-type GetLoggerWrapperArgs = {
-  readonly name: string;
-  readonly level?: keyof LogLevels;
-  readonly handlers?: Handlers;
-};
-type GetLoggerWrapper = (args: GetLoggerWrapperArgs) => GetLoggerReturn;
 
 const getLoggerWrapper: GetLoggerWrapper = ({
   name = 'root',
---
+++ src/types.ts
@@ -0,0 +1,76 @@
+// https://en.wikipedia.org/wiki/Syslog#Severity_level
+const logLevels = {
+  emerg: 0,
+  alert: 1,
+  crit: 2,
+  err: 3,
+  warning: 4,
+  notice: 5,
+  info: 6,
+  debug: 7,
+} as const;
+
+type LogLevels = typeof logLevels;
+type LoggerName = string;
+type NameChain = Readonly<LoggerName[]>;
+type Logger = {
+  readonly level: keyof LogLevels;
+  readonly name: LoggerName;
+  readonly nameChain: NameChain;
+};
+type MessageRaw = string;
+interface Message {
+  readonly raw: MessageRaw;
+  readonly level: keyof LogLevels;
+}
+interface MessageFormatted extends Message {
+  readonly formatted: string;
+}
+
+// a Filter decides if current call should be logged or not
+type Filter = (logger: Logger, message: Message) => boolean;
+
+// a Formatter formats the raw message into a string that will be logged
+type Formatter = (logger: Logger, message: Message) => MessageFormatted;
+
+// a Transporter takes the formatted message and "transports" it to console, filesystem, network, etc
+type Transporter = (logger: Logger, message: MessageFormatted) => void;
+
+// a Handler bundles Filter, Formatter and Transporter
+type Handler = {
+  readonly filter: Filter;
+  readonly formatter: Formatter;
+  readonly transporter: Transporter;
+};
+type Handlers = Readonly<Handler[]>;
+
+type GetLoggerArgs = {
+  readonly name: string;
+  readonly level: keyof LogLevels;
+  readonly handlers: Handlers;
+  readonly nameChain: NameChain;
+};
+type GetLoggerReturn = { [K in keyof LogLevels]: (msg: MessageRaw) => void } & {
+  getLogger: GetLoggerWrapper;
+};
+type GetLogger = (args: GetLoggerArgs) => GetLoggerReturn;
+type GetLoggerWrapperArgs = {
+  readonly name: string;
+  readonly level?: keyof LogLevels;
+  readonly handlers?: Handlers;
+};
+type GetLoggerWrapper = (args: GetLoggerWrapperArgs) => GetLoggerReturn;
+
+export type {
+  Filter,
+  Formatter,
+  GetLogger,
+  GetLoggerReturn,
+  GetLoggerWrapper,
+  Handler,
+  Handlers,
+  Logger,
+  Message,
+  MessageRaw,
+  Transporter,
+};
