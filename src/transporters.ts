f16ea10241993fe1d849c106b126f5f1db28521a
pabra <patrick+github-commit-2020@braune.xyz>
2020-06-24 21:10:01 +0200

    use console log functions appropriate to log level

--- src/transporters.ts
+++ src/transporters.ts
@@ -1,7 +1,31 @@
 import { Transporter } from './types';
+import { assertNever } from './utils';
 
-const consoleTransporter: Transporter = (_logger, { formatted }) => {
-  console.log(formatted);
+const consoleTransporter: Transporter = (_logger, msg) => {
+  switch (msg.level) {
+    case 'emerg':
+    case 'alert':
+    case 'crit':
+    case 'err':
+      console.error(msg.formatted);
+      break;
+
+    case 'warning':
+      console.warn(msg.formatted);
+      break;
+
+    case 'notice':
+    case 'info':
+      console.info(msg.formatted);
+      break;
+
+    case 'debug':
+      console.debug(msg.formatted);
+      break;
+
+    default:
+      assertNever(msg.level);
+  }
 };
 
 export { consoleTransporter };
